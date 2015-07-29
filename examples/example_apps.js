d5ccf1e963af5fb29d25728347430e17b02756c2
Juan Antonio Breña Moral <bren@juanantonio.info>
2015-07-29 11:50:59 +0200

    Adding model https://github.com/jabrena/cf-nodejs-client/issues/2

--- examples/example_apps.js
+++ examples/example_apps.js
@@ -3,8 +3,10 @@
 "use strict";
 
 var cloudFoundry = require("../lib/model/CloudFoundry");
+var cloudFoundryApps = require("../lib/model/Apps");
 var config = require('./config.json');
 cloudFoundry = new cloudFoundry(config.CF_API_URL);
+cloudFoundryApps = new cloudFoundryApps(config.CF_API_URL);
 
 //TODO: How to improve this idea
 process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0";
@@ -12,7 +14,9 @@ process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0";
 cloudFoundry.getInfo().then(function (result) {
     return cloudFoundry.login(result.token_endpoint,config.username,config.password);
 }).then(function (result) {
-    console.log(result);
+    return cloudFoundryApps.getApps(result.token_type,result.access_token);
+}).then(function (result) {
+    console.log(result.resources);    
 }).catch(function (reason) {
     console.error("Error: " + reason);
 });
--- examples/example_events.js
+++ examples/example_events.js
@@ -3,8 +3,10 @@
 "use strict";
 
 var cloudFoundry = require("../lib/model/CloudFoundry");
+var cloudFoundryEvents = require("../lib/model/Events");
 var config = require('./config.json');
 cloudFoundry = new cloudFoundry(config.CF_API_URL);
+cloudFoundryEvents = new cloudFoundryEvents(config.CF_API_URL);
 
 //TODO: How to improve this idea
 process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0";
@@ -12,7 +14,9 @@ process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0";
 cloudFoundry.getInfo().then(function (result) {
     return cloudFoundry.login(result.token_endpoint,config.username,config.password);
 }).then(function (result) {
-    console.log(result);
+    return cloudFoundryEvents.getEvents(result.token_type,result.access_token);
+}).then(function (result) {
+    console.log(result.resources);    
 }).catch(function (reason) {
     console.error("Error: " + reason);
 });
