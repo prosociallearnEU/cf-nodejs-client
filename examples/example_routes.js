fd7e01842da7281c6fa9eb27c9db8da9c7c26bf3
Juan Antonio Breña Moral <bren@juanantonio.info>
2015-08-06 12:58:55 +0200

    Adding the method removeRoute in Routes.js Tested.

--- examples/example_routes.js
+++ examples/example_routes.js
@@ -3,6 +3,7 @@
 "use strict";
 
 var randomWords = require('random-words');
+//var config = require('./configPivotal.json');
 var config = require('./config.json');
 var cloudFoundry = require("../lib/model/CloudFoundry");
 var cloudFoundryRoutes = require("../lib/model/Routes");
@@ -18,6 +19,8 @@ cloudFoundryApps = new cloudFoundryApps(config.CF_API_URL);
 //TODO: How to improve this idea
 process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0";
 
+var page = 1;//Pagination parameters
+var routesList = [];
 var token_endpoint = null;
 var route_guid = null;
 var domain_guid = null;
@@ -35,7 +38,7 @@ function getRoutes(){
         cloudFoundry.getInfo().then(function (result) {
             token_endpoint = result.token_endpoint;
             return cloudFoundry.login(token_endpoint,config.username,config.password).then(function (result) {
-                return cloudFoundryRoutes.getRoutes(result.token_type,result.access_token).then(function (result) {
+                return cloudFoundryRoutes.getRoutes(result.token_type,result.access_token,page).then(function (result) {
                     return new Promise(function (resolve, reject) {
                         if(result.total_results == 0){
                             return reject("No routes");
@@ -45,8 +48,11 @@ function getRoutes(){
                 });             
             }); 
         }).then(function (result) {
-            //console.log(result);
-            return resolve("OK, Test 1");
+            if(result.total_results) {
+                return resolve("OK, Test getRoutes");
+            }else{
+                return reject("KO, Test getRoutes");     
+            }
         }).catch(function (reason) {
             console.error("Error: " + reason);
             return reject("KO, Test 1");
@@ -64,7 +70,7 @@ function getRoute(){
         cloudFoundry.getInfo().then(function (result) {
             token_endpoint = result.token_endpoint;
             return cloudFoundry.login(token_endpoint,config.username,config.password).then(function (result) {
-                return cloudFoundryRoutes.getRoutes(result.token_type,result.access_token).then(function (result) {
+                return cloudFoundryRoutes.getRoutes(result.token_type,result.access_token,page).then(function (result) {
                     return new Promise(function (resolve, reject) {
                         if(result.resources.length == 0){
                             return reject();
@@ -121,15 +127,120 @@ function addRoute(){
                 return cloudFoundryRoutes.addRoute(result.token_type,result.access_token,domain_guid,space_guid,routeName);
             });
         }).then(function (result) {
-            //console.log(result); 
-            return resolve("OK, Test 3");
+            console.log("New route: ", result.entity.host , " ", result.metadata.guid);
+            route_guid = result.metadata.guid;
+            return resolve("OK, Test Add");
         }).catch(function (reason) {
             console.error("Error: " + reason);
-            return reject("KO, Test 3");
+            return reject("KO, Test Add");
         });
 
     }); 
 
+}
+
+function removeRoute(){
+
+    console.log("# Remove a route");
+    return new Promise(function (resolve, reject) {
+
+        cloudFoundry.getInfo().then(function (result) {
+            token_endpoint = result.token_endpoint;
+            return cloudFoundry.login(token_endpoint,config.username,config.password).then(function (result) {
+                return cloudFoundryRoutes.getRoutes(result.token_type,result.access_token,page).then(function (result) {
+                    return new Promise(function (resolve, reject) {
+                        if(result.total_results == 0){
+                            return reject("No routes");
+                        }
+                        return resolve(result);
+                    });
+                });             
+            });
+        }).then(function (result) {
+            //console.log(result)
+            var total = result.total_results;
+            console.log(total);
+            if(total > 0){
+                if(total > 50){
+                    for(var i = 0; i< result.resources.length; i++){
+                        //console.log(i, " " ,result.resources[i].entity.host, "  ", result.resources[i].metadata.guid);
+                        
+                        routesList.push({
+                            'route': result.resources[i].entity.host,
+                            'guid': result.resources[i].metadata.guid
+                        });
+                    }
+
+                    //TODO: How to do a Loop with promises to paginate?
+                    //Manual pagination for second page
+                    page = 2;
+                    return cloudFoundry.login(token_endpoint,config.username,config.password).then(function (result) {
+                        return cloudFoundryRoutes.getRoutes(result.token_type,result.access_token,page).then(function (result) {
+                            return new Promise(function (resolve, reject) {
+                                if(result.total_results == 0){
+                                    return reject("No routes");
+                                }
+                                //List
+                                for(var i = 0; i< result.resources.length; i++){
+                                    //console.log(i, " " ,result.resources[i].entity.host, "  ", result.resources[i].metadata.guid);
+                                    routesList.push({
+                                        'route': result.resources[i].entity.host,
+                                        'guid': result.resources[i].metadata.guid
+                                    });
+                                }
+
+                                //Show
+                                for(var i = 0; i< routesList.length; i++){
+                                    if(routesList[i].route === routeName){
+                                        console.log(i + " " + routesList[i].route + " " + routesList[i].guid);
+                                        console.log("FOUND");
+                                        break;
+                                    }
+                                }
+
+                                return resolve(result);
+                            });
+                        });             
+                    }); 
+                }else{
+                    for(var i = 0; i< result.resources.length; i++){
+                        //console.log(i, " " ,result.resources[i].entity.host, "  ", result.resources[i].metadata.guid);
+                        routesList.push({
+                            'route': result.resources[i].entity.host,
+                            'guid': result.resources[i].metadata.guid
+                        });
+                    }
+                }
+
+                return reject("KO, Test 1");
+            }else{
+                return reject("KO, Test 1");  
+            }
+        }).then(function (result) {
+            return cloudFoundry.login(token_endpoint,config.username,config.password).then(function (result) {   
+                return cloudFoundryRoutes.deleteRoute(result.token_type,result.access_token,route_guid);  
+            });
+        }).then(function (result) {
+            return cloudFoundry.login(token_endpoint,config.username,config.password).then(function (result) {
+                return cloudFoundryRoutes.getRoutes(result.token_type,result.access_token,page).then(function (result) {
+                    return new Promise(function (resolve, reject) {
+                        if(result.total_results == 0){
+                            return reject("No routes");
+                        }
+                        var total = result.total_results;
+                        console.log(total);
+                        return resolve(result);
+                    });
+                });             
+            });
+        }).then(function (result) {
+            return resolve("OK, Test Remove");   
+        }).catch(function (reason) {
+            console.error("Error: " + reason);
+            return reject("KO, Test Remove");
+        });
+
+    });
 }
 
 //Check a route
@@ -153,7 +264,8 @@ function checkRoute(){
                 return cloudFoundryRoutes.checkRoute(result.token_type,result.access_token,"routedemo1",domain_guid);  
             });
         }).then(function (result) {
-            //console.log(result);
+            //TODO: No inference ??
+            console.log(result);
             return resolve("OK, Test 4");  
         }).catch(function (reason) { 
             console.error("Error: " + reason);
@@ -228,10 +340,12 @@ function acceptRoute(){
 */
 
 //Examples about Routes executed in an Order.
-getRoutes().then(function (result) {
-    return getRoute();
+addRoute().then(function (result) {
+    return removeRoute();
+}).then(function (result) {    
+    return getRoutes();
 }).then(function (result) {
-    return addRoute();
+    return getRoute();
 }).then(function (result) { 
     return checkRoute();
 }).then(function (result) {     
