453f7e2b56d1782b58d2b2e8bf2932368806b4a8
Juan Antonio Breña Moral <bren@juanantonio.info>
2015-08-10 15:06:50 +0200

    Minor change

--- index.js
+++ index.js
@@ -16,6 +16,13 @@ var used = [],
 exports.version = '0.0.4';
 
 /**
+ * Support for Cloud Foundry 
+ * @type {[type]}
+ */
+var CloudFoundry = require('./lib/model/CloudFoundry');
+exports.CloudFoundry = CloudFoundry;
+
+/**
  * Support for Apps
  * @type {[type]}
  */
@@ -23,8 +30,50 @@ var Apps = require('./lib/model/Apps');
 exports.Apps = Apps;
 
 /**
- * Support for Cloud Foundry 
+ * Support for Domains
  * @type {[type]}
  */
-var CloudFoundry = require('./lib/model/CloudFoundry');
-exports.CloudFoundry = CloudFoundry;
+var Domains = require('./lib/model/Domains');
+exports.Domains = Domains;
+
+/**
+ * Support for Events
+ * @type {[type]}
+ */
+var Events = require('./lib/model/Events');
+exports.Events = Events;
+
+/**
+ * Support for Jobs
+ * @type {[type]}
+ */
+var Jobs = require('./lib/model/Jobs');
+exports.Jobs = Jobs;
+
+/**
+ * Support for Organizations
+ * @type {[type]}
+ */
+var Organizations = require('./lib/model/Organizations');
+exports.Organizations = Organizations;
+
+/**
+ * Support for Routes
+ * @type {[type]}
+ */
+var Routes = require('./lib/model/Routes');
+exports.Routes = Routes;
+
+/**
+ * Support for Spaces
+ * @type {[type]}
+ */
+var Spaces = require('./lib/model/Spaces');
+exports.Spaces = Spaces;
+
+/**
+ * Support for Stacks
+ * @type {[type]}
+ */
+var Stacks = require('./lib/model/Stacks');
+exports.Stacks = Stacks;
