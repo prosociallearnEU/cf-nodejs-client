f90f26567de1e2723b4734f46f04d620d370ce79
Juan Antonio Breña Moral <bren@juanantonio.info>
2015-09-02 15:11:10 +0200

    Minor change

--- test/lib/model/CloudFoundryTests.js
+++ test/lib/model/CloudFoundryTests.js
@@ -10,23 +10,42 @@ chai.use(chaiAsPromised);
 var nconf = require('nconf');
 nconf.argv().env().file({ file: 'config.json' });
 
-var cf_api_url = nconf.get('CF_API_URL'),
+var endPoint = nconf.get('CF_API_URL'),
     username = nconf.get('username'),
     password = nconf.get('password');
 
 var CloudFoundry = require("../../../lib/model/CloudFoundry");
-CloudFoundry = new CloudFoundry(cf_api_url);
+CloudFoundry = new CloudFoundry();
 
 process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0";
 
 describe("Cloud Foundry", function () {
 
     it("The connection with the PaaS is OK", function () {
+
+        CloudFoundry.setEndPoint(endPoint);
+
         return expect(CloudFoundry.getInfo()).eventually.property("name", "vcap");
     });
 
     it("The authentication with the PaaS is OK", function () {
         this.timeout(2500);
+
+        CloudFoundry.setEndPoint(endPoint);
+
+        var token_endpoint = null;
+        return CloudFoundry.getInfo().then(function (result) {
+            token_endpoint = result.token_endpoint;
+            return CloudFoundry.login(token_endpoint, username, password);
+        }).then(function (result) {
+            expect(result.token_type).to.equal("bearer");
+        });
+    });
+
+    it("Use the constructor without the EndPoint", function () {
+        this.timeout(2500);
+
+        CloudFoundry.setEndPoint(endPoint);
 
         var token_endpoint = null;
         return CloudFoundry.getInfo().then(function (result) {
