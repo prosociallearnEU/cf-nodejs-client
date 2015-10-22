8d0919843f8f887d81ff47dfa8f2449a92e21d22
Juan Antonio Breña Moral <bren@juanantonio.info>
2015-10-22 15:48:29 +0200

    https://github.com/prosociallearnEU/cf-nodejs-client/issues/87

--- test/lib/model/BuildPacksTests.js
+++ test/lib/model/BuildPacksTests.js
@@ -8,7 +8,7 @@ var chai = require("chai"),
 var Buildpacks = require("../../../lib/model/BuildPacks");
 Buildpacks = new Buildpacks();
 
-describe("Cloud Foundry Buildpacks", function () {
+describe.only("Cloud Foundry Buildpacks", function () {
 
     it("Exist a support for a Node.js project", function () {
         expect(Buildpacks.get("nodejs")).to.equal("https://github.com/cloudfoundry/nodejs-buildpack");
@@ -16,6 +16,10 @@ describe("Cloud Foundry Buildpacks", function () {
 
     it("Exist a support for a Static project", function () {
         expect(Buildpacks.get("static")).to.equal("https://github.com/cloudfoundry/staticfile-buildpack");
+    });
+
+    it("Exist a support for a Java project", function () {
+        expect(Buildpacks.get("java")).to.equal("https://github.com/cloudfoundry/java-buildpack");
     });
 
     it("Unknown Buildpacks handling", function () {
